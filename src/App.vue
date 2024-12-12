ce03944182ca8b222aa1cf0f02e9bdbd533ee0c0
arielramos03 <103594586+ramoscariel@users.noreply.github.com>
2024-12-11 20:29:02 -0500

    Update page header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
-<script setup>
-
-</script>
-
 <template>
-<h1>Notes</h1>
-<RouterView></RouterView>
+    <header>
+        <router-link to="/">
+            <h1>Notes</h1>
+        </router-link>
+        <div v-if="!isLoggedIn">
+            <router-link to="/login">Log in</router-link>
+            <router-link to="/register">Register</router-link>
+        </div>
+        <div v-else>
+            <!--<router-link to="/profile">Profile</router-link>-->
+            <button @click="logout">Log out</button>
+        </div>
+    </header>
+    <RouterView></RouterView>
 </template>
+
+<script setup>
+import { useRouter, useRoute } from 'vue-router';
+import { ref, onMounted, watch } from 'vue';
+
+const route = useRoute()
+const router = useRouter();
+const isLoggedIn = ref(false)
+
+const checkLoginStatus = () => {
+  isLoggedIn.value = !!localStorage.getItem('token')
+}
+
+const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user_id');
+    isLoggedIn.value = false;
+    router.push('/');
+}
+
+watch(route, () => {
+  checkLoginStatus()
+});
+
+onMounted(() => {
+    checkLoginStatus();
+});
+</script>
